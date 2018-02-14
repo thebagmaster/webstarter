4253c9d44a6431b195ce8024b9f196b0ef180590
root <root@archnth.localhomain>
2018-02-14 13:28:47 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')));
+
+app.use(express.static('public'));
+
+app.listen(80, () => console.log('Server Started'))
